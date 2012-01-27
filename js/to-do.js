2af9deadbe9008df1b10ca0cb706bb7c106b7df0
adixime <adixime@gmail.com>
2012-01-27 12:41:13 -0500

    half way

---
+++ js/to-do.js
@@ -0,0 +1,24 @@
+var newlist;
+
+var createList = function (ev) {
+	
+	newlist = document.createElement('li'); // .createElement() makes a new empty HTML tag
+	
+	newlist.className = 'listed'; //.className allows us to manipulate the classes of a tag
+	
+	document.getElementById('ul').appendChild(newlist);	 //.appendChild() will add an element to the HTML file
+	
+	newlist.innerHTML = document.getElementById('text').value;
+
+};
+
+document.getElementById('listitem').addEventListener('click', createList, false);
+
+
+document.documentElement.addEventListener('click', function (ev) {
+	if (ev.target.tagName == 'LI') {
+		ev.target.className = 'listed';
+	//	list = ev.target;
+		
+	}
+}, false);
